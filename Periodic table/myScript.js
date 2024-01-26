770df1b1dbd2a67f016de61438dbe1044049aa36
Steven Gestl <105674896+StevenGestl@users.noreply.github.com>
2024-01-26 06:10:40 -0500

    Add files via upload
    Added array data for the elements, added functions for element display popup

--- Periodic table/myScript.js
+++ Periodic table/myScript.js
@@ -95,6 +95,7 @@ function franciumPress() {
   document.getElementById("demo2").innerHTML = francium[2];
 
 }
+/* */
 
 function berylliumPress() {
   var T = document.getElementById("display_element");
@@ -149,6 +150,7 @@ function radiumPress() {
   document.getElementById("demo2").innerHTML = radium[2];
 
 }
+/* */
 
 function scandiumPress() {
   var T = document.getElementById("display_element");
@@ -185,14 +187,400 @@ function actiniumPress() {
   document.getElementById("demo2").innerHTML = actinium[2];
  
 }
+/* */
+function titaniumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + titanium[0];
+  document.getElementById("demo1").innerHTML = titanium[1];
+  document.getElementById("demo2").innerHTML = titanium[2];
+ 
+}
 
+function zirconiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + zirconium[0];
+  document.getElementById("demo1").innerHTML = zirconium[1];
+  document.getElementById("demo2").innerHTML = zirconium[2];
+ 
+}
+
+function hafniumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + hafnium[0];
+  document.getElementById("demo1").innerHTML = hafnium[1];
+  document.getElementById("demo2").innerHTML = hafnium[2];
+ 
+}
+
+function rutherfordiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + rutherfordium[0];
+  document.getElementById("demo1").innerHTML = rutherfordium[1];
+  document.getElementById("demo2").innerHTML = rutherfordium[2];
+ 
+}
+/* */
+
+function vanadiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + vanadium[0];
+  document.getElementById("demo1").innerHTML = vanadium[1];
+  document.getElementById("demo2").innerHTML = vanadium[2];
+ 
+}
+
+function niobiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + niobium[0];
+  document.getElementById("demo1").innerHTML = niobium[1];
+  document.getElementById("demo2").innerHTML = niobium[2];
+ 
+}
+
+function tantalumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + tantalum[0];
+  document.getElementById("demo1").innerHTML = tantalum[1];
+  document.getElementById("demo2").innerHTML = tantalum[2];
+ 
+}
+
+function dubniumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + dubnium[0];
+  document.getElementById("demo1").innerHTML = dubnium[1];
+  document.getElementById("demo2").innerHTML = dubnium[2];
+ 
+}
+/* */
+
+function chromiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + chromium[0];
+  document.getElementById("demo1").innerHTML = chromium[1];
+  document.getElementById("demo2").innerHTML = chromium[2];
+ 
+}
+
+function molybdenumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + molybdenum[0];
+  document.getElementById("demo1").innerHTML = molybdenum[1];
+  document.getElementById("demo2").innerHTML = molybdenum[2];
+ 
+}
+
+function tungstenPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + tungsten[0];
+  document.getElementById("demo1").innerHTML = tungsten[1];
+  document.getElementById("demo2").innerHTML = tungsten[2];
+ 
+}
+
+function seaborgiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + seaborgium[0];
+  document.getElementById("demo1").innerHTML = seaborgium[1];
+  document.getElementById("demo2").innerHTML = seaborgium[2];
+ 
+}
+/* */
+
+function manganesePress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + manganese[0];
+  document.getElementById("demo1").innerHTML = manganese[1];
+  document.getElementById("demo2").innerHTML = manganese[2];
+ 
+}
+
+function technetiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + technetium[0];
+  document.getElementById("demo1").innerHTML = technetium[1];
+  document.getElementById("demo2").innerHTML = technetium[2];
+ 
+}
+
+function rheniumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + rhenium[0];
+  document.getElementById("demo1").innerHTML = rhenium[1];
+  document.getElementById("demo2").innerHTML = rhenium[2];
+ 
+}
+
+function bohriumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + bohrium[0];
+  document.getElementById("demo1").innerHTML = bohrium[1];
+  document.getElementById("demo2").innerHTML = bohrium[2];
+ 
+}
+/* */
+
+function ironPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + iron[0];
+  document.getElementById("demo1").innerHTML = iron[1];
+  document.getElementById("demo2").innerHTML = iron[2];
+ 
+}
+
+function rutheniumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + ruthenium[0];
+  document.getElementById("demo1").innerHTML = ruthenium[1];
+  document.getElementById("demo2").innerHTML = ruthenium[2];
+ 
+}
+
+function osmiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + osmium[0];
+  document.getElementById("demo1").innerHTML = osmium[1];
+  document.getElementById("demo2").innerHTML = osmium[2];
+ 
+}
+
+function hassiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + hassium[0];
+  document.getElementById("demo1").innerHTML = hassium[1];
+  document.getElementById("demo2").innerHTML = hassium[2];
+ 
+}
+/* */
+
+function cobaltPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + cobalt[0];
+  document.getElementById("demo1").innerHTML = cobalt[1];
+  document.getElementById("demo2").innerHTML = cobalt[2];
+ 
+}
+
+function rhodiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + rhodium[0];
+  document.getElementById("demo1").innerHTML = rhodium[1];
+  document.getElementById("demo2").innerHTML = rhodium[2];
+ 
+}
+
+function iridiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + iridium[0];
+  document.getElementById("demo1").innerHTML = iridium[1];
+  document.getElementById("demo2").innerHTML = iridium[2];
+ 
+}
+
+function meitneriumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + meitnerium[0];
+  document.getElementById("demo1").innerHTML = meitnerium[1];
+  document.getElementById("demo2").innerHTML = meitnerium[2];
+ 
+}
+/* */
+function nickelPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + nickel[0];
+  document.getElementById("demo1").innerHTML = nickel[1];
+  document.getElementById("demo2").innerHTML = nickel[2];
+ 
+}
+
+function palladiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + palladium[0];
+  document.getElementById("demo1").innerHTML = palladium[1];
+  document.getElementById("demo2").innerHTML = palladium[2];
+ 
+}
+
+function platinumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + platinum[0];
+  document.getElementById("demo1").innerHTML = platinum[1];
+  document.getElementById("demo2").innerHTML = platinum[2];
+ 
+}
+
+function darmstadtiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + darmstadtium[0];
+  document.getElementById("demo1").innerHTML = darmstadtium[1];
+  document.getElementById("demo2").innerHTML = darmstadtium[2];
+ 
+}
+/* */
+function copperPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + copper[0];
+  document.getElementById("demo1").innerHTML = copper[1];
+  document.getElementById("demo2").innerHTML = copper[2];
+ 
+}
+
+function silverPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + silver[0];
+  document.getElementById("demo1").innerHTML = silver[1];
+  document.getElementById("demo2").innerHTML = silver[2];
+ 
+}
+
+function goldPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + gold[0];
+  document.getElementById("demo1").innerHTML = gold[1];
+  document.getElementById("demo2").innerHTML = gold[2];
+ 
+}
+
+function roentgeniumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + roentgenium[0];
+  document.getElementById("demo1").innerHTML = roentgenium[1];
+  document.getElementById("demo2").innerHTML = roentgenium[2];
+ 
+}
+/* */
+
+function zincPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + zinc[0];
+  document.getElementById("demo1").innerHTML = zinc[1];
+  document.getElementById("demo2").innerHTML = zinc[2];
+ 
+}
+
+function cadmiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + cadmium[0];
+  document.getElementById("demo1").innerHTML = cadmium[1];
+  document.getElementById("demo2").innerHTML = cadmium[2];
+ 
+}
+
+function mercuryPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + mercury[0];
+  document.getElementById("demo1").innerHTML = mercury[1];
+  document.getElementById("demo2").innerHTML = mercury[2];
+ 
+}
+
+function coperniciumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + copernicium[0];
+  document.getElementById("demo1").innerHTML = copernicium[1];
+  document.getElementById("demo2").innerHTML = copernicium[2];
+ 
+}
+/* */
+
+function boronPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + boron[0];
+  document.getElementById("demo1").innerHTML = boron[1];
+  document.getElementById("demo2").innerHTML = boron[2];
+ 
+}
+
+function aluminiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + aluminium[0];
+  document.getElementById("demo1").innerHTML = aluminium[1];
+  document.getElementById("demo2").innerHTML = aluminium[2];
+ 
+}
+
+function galliumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + gallium[0];
+  document.getElementById("demo1").innerHTML = gallium[1];
+  document.getElementById("demo2").innerHTML = gallium[2];
+ 
+}
+
+function indiumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + indium[0];
+  document.getElementById("demo1").innerHTML = indium[1];
+  document.getElementById("demo2").innerHTML = indium[2];
+ 
+}
+
+function thalliumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + thallium[0];
+  document.getElementById("demo1").innerHTML = thallium[1];
+  document.getElementById("demo2").innerHTML = thallium[2];
+ 
+}
+
+function nihoniumPress() {
+  var T = document.getElementById("display_element");
+  T.style.display = "block";
+  document.getElementById("demo").innerHTML = "Name: " + nihonium[0];
+  document.getElementById("demo1").innerHTML = nihonium[1];
+  document.getElementById("demo2").innerHTML = nihonium[2];
+ 
+}
+/* */
 
 /* Pulls array data from helium const and places into DOM */
 function heliumPress() {
   document.getElementById("demo1").innerHTML = helium;
 }
 
-/* Array data for various elements in the table */
+/* Array data for various elements in the table
+const element = [name, symbol, atomic number, atomic mass ] */
 const hydrogen = ["Hydrogen", "H", 1, 1.008];
 const lithium = ["Lithium", "Li", 3, 6.9410];
 const sodium = ["Sodium", "Na", 11, 22.990];
@@ -271,10 +659,66 @@ const tin = ["Tin", "Sn", 50, 118.71];
 const lead = ["Lead", "Pb", 82, 207.20];
 const flerovium = ["Flerovium", "Fl", 114, 289];
 /* */
-
+const nitrogen = ["Nitrogen", "N", 7, 14.007];
+const phosphorus = ["Phosphorus", "P", 15, 30.974];
+const arsenic = ["Arsenic", "As", 33, 74.922];
+const antimony = ["Antimony", "Sb", 51, 121.76];
+const bismuth = ["Bismuth", "Bi", 83, 208.98];
+const moscovium = ["Moscovium", "Mc", 115, 289];
+/* */
+const oxygen = ["Oxygen", "O", 8, 15.999];
+const sulfur = ["Sulfur", "S", 16, 32.065];
+const selenium = ["Selenium", "Se", 34, 78.960];
+const tellurium = ["Tellurium", "Te", 52, 127.60];
+const polonium = ["Polonium", "Po", 84, 209];
+const livermorium = ["Livermorium", "Lv", 116, 293];
+/* */
+const fluorine = ["Fluorine", "F", 9, 18.998];
+const chlorine = ["Chlorine", "Cl", 17, 35.453];
+const bromine = ["Bromine", "Br", 35, 79.904]
+const iodine = ["Iodine", "I", 53, 126.90];
+const astatine = ["Astatine", "At", 85, 210];
+const tennessine = ["Tennessine", "Ts", 117, 294];
+/* */
+const helium = ["Helium", "He", 2, 4.0026];
+const neon = ["Neon", "Ne", 10, 20.180];
+const argon = ["Argon", "Ar", 18, 39.948];
+const krypton = ["Krypton", "Kr", 36, 83.798];
+const xenon = ["Xenon", "Xe", 54, 131.29];
+const radon = ["Radon", "Rn", 86, 222];
+const oganesson = ["Oganesson", "Og", 118, 294];
+/* */
+const cerium = ["Cerium", "Ce", 58, 140.12];
+const praseodymium = ["Praseodymium", "Pr", 59, 140.91];
+const neodymium = ["Neodymium", "Nd", 60, 144.24];
+const samarium = ["Samarium", "Sm", 62, 150.36];
+const europium = ["Europium", "Eu", 63, 151.96];
+const gadolinium = ["Gadolinium", "Gd", 64, 157.25];
+const terbium = ["Terbium", "Tb", 65, 158.93];
+const dysprosium = ["Dysprosium", "Dy", 66, 162.5];
+const holmium = ["Holmium", "Ho", 67, 164.93];
+const erbium = ["Erbium", "Er", 68, 167.26];
+const thulium = ["Thulium", "Tm", 69, 168.93];
+const ytterbium = ["Ytterbium", "yb", 70, 173.04];
+const lutetium = ["Lutetium", "Lu", 71, 174.97];
+/* */
+const thorium = ["Thorium", "Th", 90, 232.04];
+const protactinium = ["Protactinium", "Pa", 91, 231.04];
+const uranium = ["Uranium", "U", 92, 238.03];
+const neptunium = ["Neptunium", "Np", 93, 237.05];
+const plutonium = ["Plutonium", "Pu", 94, 244];
+const americium = ["Americium", "Am", 95, 243];
+const curium = ["Curium", "Cm", 96, 247];
+const berkelium = ["Berkelium", "Bk", 97, 247];
+const californium = ["Californium", "Cf", 98, 251];
+const einsteinium = ["Einsteinium", "Es", 99, 252];
+const fermium = ["Fermium", "Fm", 100, 257];
+const mendelevium = ["Mendelevium", "Md", 101, 258];
+const nobelium = ["Nobelium", "No", 102, 259];
+const lawrencium = ["Lawrencium", "Lr", 103, 262];
 
 const hydrogenString = hydrogen.join(" , ");
-const helium = ["Helium", "Gas"];
+
 
 /* displays div with data from hydrogenPress function */
 function testFunction() {
